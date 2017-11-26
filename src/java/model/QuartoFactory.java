3d072e69b9c3ddaadffbeb7b77be84fb97c0193e
matheusPeresDeAraujo <matheus.peres.araujo@gmail.com>
2017-11-26 13:33:01 -0200

    add-quartoFactory

---
+++ src/java/model/QuartoFactory.java
@@ -0,0 +1,19 @@
+package model;
+
+public class QuartoFactory {
+    public static Quarto create(String tipo){
+        Quarto quartoObject =  null;
+        String nomeClasse = "model.Quarto"+tipo;
+        Class classe = null;
+        Object objeto = null;
+        try{
+            classe = Class.forName(nomeClasse);
+            objeto = classe.newInstance();
+        }catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex){
+            return null;
+        }
+        if(!(objeto instanceof Quarto)) return null;
+        quartoObject = (Quarto) objeto;
+        return quartoObject;
+    }
+}
